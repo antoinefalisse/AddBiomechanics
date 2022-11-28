16f5b0e023e3c81f4c4077565092f12afc959df9
Antoine Falisse <antoinefalisse@gmail.com>
2022-11-28 15:25:41 -0800

    minor

--- server/engine/first.sh
+++ server/engine/first.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for n in Subj03 Subj04 Subj05 Subj06 Subj07 Subj08;
+for n in 102;
 # for n in 150;
 do
 	# if [ ! -d "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/cmu_dataset/$n/osim_results" ]; then	
@@ -11,7 +11,11 @@ do
 	
 	# sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/cycling_dataset/P$n/_results.json "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/cycling_anthony_cleaned/P$n/osim_results"
 	
-	sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/balance_dataset/$n/osim_results "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/balance_dataset_cleaned/$n"
+	# sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/balance_dataset/$n/osim_results "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/balance_dataset_cleaned/$n"
 	
-	sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/balance_dataset/$n/_results.json "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/balance_dataset_cleaned/$n/osim_results"
+	# sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/balance_dataset/$n/_results.json "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/balance_dataset_cleaned/$n/osim_results"
+	
+	sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/myer_dataset/PreTesting_2017_fall/anmt$n/osim_results "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/myer_dataset_cleaned/PreTesting_2017_fall/ANMT$n_Base_08_30_2017"
+	
+	
 done
--- server/engine/second.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sshpass -p 'Stanford!' scp -r clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/cycling_dataset/P020/osim_results '/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/cycling_anthony_cleaned/P020'
-
-sshpass -p 'Stanford!' scp clarkadmin@171.65.103.222:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/cycling_dataset/P020/_results.json '/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/cycling_anthony_cleaned/P020'
