460c710dd6e1e99c7bb310ae0fd7a07edcfc99a9
Antoine Falisse <antoinefalisse@gmail.com>
2023-05-08 20:12:10 -0700

    backup

--- server/engine/copy_data.sh
+++ server/engine/copy_data.sh
@@ -33,15 +33,15 @@
 #     sshpass -p 'Stanford!' scp -r clarkadmin@171.65.102.146:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/${dataset}/$n/osim_results/* "/mnt/g/My Drive/Projects/openpose-augmenter/Data_opensim/${dataset}/test/$n/osim_results"
 # done;
 
-# dataset="totalcapture"
-# for n in s1 s2 s3 s4 s5 #subject02 subject03 subject04 subject08 subject10 subject11 subject17 subject19 subject20;
-# # for n in Subject1
-# do
-#     mkdir -p "/mnt/g/My Drive/Projects/TomVW_IMU/TotalCapture/trc/$n/osim_results"
-#     sshpass -p 'Stanford!1' scp -r clarkadmin@171.65.102.45:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/${dataset}/$n/osim_results/* "/mnt/g/My Drive/Projects/TomVW_IMU/TotalCapture/trc/$n/osim_results"
-# done;
+dataset="totalcapture_2"
+for n in s1_acting1 #subject02 subject03 subject04 subject08 subject10 subject11 subject17 subject19 subject20;
+# for n in Subject1
+do
+    mkdir -p "/mnt/c/Users/antoi/Documents/MyRepositories/AddBiomechanics/server/data/totalcapture_2/$n/osim_results"
+    sshpass -p 'Stanford!1' scp -r clarkadmin@171.65.102.45:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/${dataset}/$n/osim_results/* "/mnt/c/Users/antoi/Documents/MyRepositories/AddBiomechanics/server/data/totalcapture_2/$n/osim_results"
+done;
 
 
-# # Copy data to Linux machine
-dataset="totalcapture"
-sshpass -p 'Stanford!1' scp -r /mnt/c/Users/antoi/Documents/MyRepositories/AddBiomechanics/server/data/$dataset/* "clarkadmin@171.65.102.45:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/$dataset/"
+# # # Copy data to Linux machine
+# dataset="totalcapture_2"
+# sshpass -p 'Stanford!1' scp -r /mnt/c/Users/antoi/Documents/MyRepositories/AddBiomechanics/server/data/$dataset/* "clarkadmin@171.65.102.45:Documents/MyRepositories_Antoine/AddBiomechanics/server/data/$dataset/"
